ed5117b4e5444c4bdb294b11020674c5453e92a6
haticedenizz <haticedenizz5627@gmail.com>
2022-03-22 10:56:30 +0300

    Add project files.

---
+++ AjaxDosyaGonder/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxDosyaGonder.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult DosyaGonder(HttpPostedFileBase file)
+        {
+            if (file!=null)
+            {
+                if (Directory.Exists(Server.MapPath("~/dosyalar")) == false)
+                    Directory.CreateDirectory(Server.MapPath("~/dosyalar"));
+
+                file.SaveAs(Server.MapPath("~/dosyalar") + file.FileName);
+                return Json(new { mesaj = "Dosya Yüklendi" });
+            }
+            return Json(new { mesaj = "Dosya Seçilmedi" });
+        }
+    }
+}
